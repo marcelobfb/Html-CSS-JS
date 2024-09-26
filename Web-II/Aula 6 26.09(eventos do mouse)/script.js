e1b7bc7c3d6650a098ee22f546cbaf77c9703e30
marcelobfb <marcelobouchardet@hotmail.com>
2024-09-26 10:56:04 -0300

    up

--- Web-II/Aula 6 26.09(eventos do mouse)/script.js
+++ Web-II/Aula 6 26.09(eventos do mouse)/script.js
@@ -35,11 +35,37 @@ document.querySelector("#btn1").addEventListener("click", function () {
 });
 
 document.querySelector("#btn2").addEventListener("click", function () {
-    document.querySelector("#txt2").style.display = document.querySelector("#txt2").style.display == "none" ? "block" : "none";
+    document.querySelector("#txt2").style.display =
+        document.querySelector("#txt2").style.display == "none"
+            ? "block"
+            : "none";
 });
 
-document.querySelector("#btn3").addEventListener('click',function(){
-    var novo=document.createElement('p')
-    novo.textContent='Novo valor Inserido no container'
-    document.querySelector('#container').appendChild(novo)
+document.querySelector("#btn3").addEventListener("click", function () {
+    var novo = document.createElement("p");
+    novo.textContent = "Novo valor Inserido no container";
+    document.querySelector("#container").appendChild(novo);
+});
+
+document.getElementById("btn4").addEventListener("dblclick", function () {
+    document.getElementById("txt6").innerHTML =
+        "Você clicou <strong>duas</strong> vezes!";
+});
+
+document.querySelector("#j").addEventListener('mousedown',function(){
+    this.innerText='Você pressionou o botão do mouse'
+    this.style.backgroundColor='green'
+})
+document.querySelector("#j").addEventListener('mouseup',function(){
+    this.innerText='Você soltou o botão do mouse'
+    this.style.backgroundColor='brown'
+})
+
+document.querySelector('#btn5').addEventListener('mouseover',function(){
+    this.style.height='150px'
+    this.style.width='150px'
+})
+document.querySelector('#btn5').addEventListener('mouseout',function(){
+    this.style.height='50px'
+    this.style.width='100px'
 })
