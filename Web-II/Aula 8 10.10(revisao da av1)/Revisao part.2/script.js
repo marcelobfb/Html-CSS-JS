a3e3ed2644ef50ca4feb024acb6cf4ac100d00da
marcelobfb <marcelobouchardet@hotmail.com>
2024-10-10 11:03:12 -0300

    up

--- Web-II/Aula 8 10.10(revisao da av1)/Revisao part.2/script.js
+++ Web-II/Aula 8 10.10(revisao da av1)/Revisao part.2/script.js
@@ -17,19 +17,18 @@
 
 // 9)
 
-distancia=prompt("Qual a distancia em km: ")
-switch (true){
-    case (distancia<=100):
-        custo=distancia*10
-        document.write(`<br>O custo de envio é R$${custo.toFixed(2)}`)
-        break
-    case (distancia>100&&distancia<=300):
-        custo=distancia*8
-        document.write(`<br>O custo de envio é R$${custo.toFixed(2)}`)
-        break
-    case (distancia>300):
-        custo=distancia*6
-        document.write(`<br>O custo de envio é R$${custo.toFixed(2)}`)
-        break
+distancia = prompt("Qual a distancia em km: ");
+switch (true) {
+    case distancia <= 100:
+        custo = distancia * 10;
+        document.write(`<br>O custo de envio é R$${custo.toFixed(2)}`);
+        break;
+    case distancia > 100 && distancia <= 300:
+        custo = distancia * 8;
+        document.write(`<br>O custo de envio é R$${custo.toFixed(2)}`);
+        break;
+    case distancia > 300:
+        custo = distancia * 6;
+        document.write(`<br>O custo de envio é R$${custo.toFixed(2)}`);
+        break;
 }
-
