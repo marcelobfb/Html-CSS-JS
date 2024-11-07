c16c1b918272df9697b00d8ba85bbf5adf528e6c
marcelobfb <marcelobouchardet@hotmail.com>
2024-11-07 11:11:25 -0300

    aula 9 07.11

--- Web-II/Aula 9 07.11(slider e card)/script.js
+++ Web-II/Aula 9 07.11(slider e card)/script.js
@@ -1,23 +1,23 @@
-var slides=['./imgs/doc.png','./imgs/mate.png','./imgs/relampago.png','./imgs/sally.png']
-var tam=slides.length
-var sAtual=0
-var tmpslider
+var slides = ['./imgs/doc.png', './imgs/mate.png', './imgs/relampago.png', './imgs/sally.png'];
+var tam = slides.length;
+var sAtual = 0;
+var tmpslider;
 
-function trocaAutomatica(){
-    sAtual++
-    if(sAtual >= tam){
-        sAtual=0
+function trocaAutomatica() {
+    sAtual++;
+    if (sAtual >= tam) {
+        sAtual = 0;
     }
-    document.querySelector("#dvSlider").style.backgroundImage=`url('${slides[sAtual]}')`
+    document.querySelector("#dvSlider").style.backgroundImage = `url('${slides[sAtual]}')`;
 }
 
-function iniciarSlider(){
-    document.querySelector("#dvSlider").style.backgroundImage=`url('${slides[sAtual]}')`
-    tmpslider= setInterval(trocaAutomatica,2000)
+function iniciarSlider() {
+    document.querySelector("#dvSlider").style.backgroundImage = `url('${slides[sAtual]}')`;
+    tmpslider = setInterval(trocaAutomatica, 2000);
 }
 
-function parar(){
-    clearInterval(tmpslider)
+function parar() {
+    clearInterval(tmpslider);
 }
 
 function troca(direcao) {
@@ -30,13 +30,24 @@ function troca(direcao) {
     document.querySelector("#dvSlider").style.backgroundImage = `url('${slides[sAtual]}')`;
 }
 
-function expandCard(card){
-    card.style.transform = 'scale(1.05)'
-    card.style.boxShadow = '0 8px 16px rgba(0,0,0,0.3)'
+function expandCard(card) {
+    card.style.transform = 'scale(1.05)';
+    card.style.boxShadow = '0 8px 16px rgba(0, 0, 0, 0.3)';
 }
 
-function shinkCard(card){
-    card.style.transform = 'scale(1)'
-    card.style.boxShadow = 'none'
+function shinkCard(card) {
+    card.style.transform = 'scale(1)';
+    card.style.boxShadow = 'none';
 }
 
+function rotateCard(card) {
+    card.style.transform = "rotate(20deg)";
+}
+
+function resetCard(card) {
+    card.style.transform = "rotate(0deg)";
+}
+
+function showMoreInfo() {
+    alert("Mais informação");
+}
