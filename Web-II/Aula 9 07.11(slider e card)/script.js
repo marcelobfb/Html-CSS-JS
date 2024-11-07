81da34c0c7c975b9f3e8d499f5a3194b330dfeb0
marcelobfb <marcelobouchardet@hotmail.com>
2024-11-07 10:34:41 -0300

    aula 7 web

---
+++ Web-II/Aula 9 07.11(slider e card)/script.js
@@ -0,0 +1,42 @@
+var slides=['./imgs/doc.png','./imgs/mate.png','./imgs/relampago.png','./imgs/sally.png']
+var tam=slides.length
+var sAtual=0
+var tmpslider
+
+function trocaAutomatica(){
+    sAtual++
+    if(sAtual >= tam){
+        sAtual=0
+    }
+    document.querySelector("#dvSlider").style.backgroundImage=`url('${slides[sAtual]}')`
+}
+
+function iniciarSlider(){
+    document.querySelector("#dvSlider").style.backgroundImage=`url('${slides[sAtual]}')`
+    tmpslider= setInterval(trocaAutomatica,2000)
+}
+
+function parar(){
+    clearInterval(tmpslider)
+}
+
+function troca(direcao) {
+    sAtual += direcao;
+    if (sAtual >= tam) {
+        sAtual = 0;
+    } else if (sAtual < 0) {
+        sAtual = tam - 1;
+    }
+    document.querySelector("#dvSlider").style.backgroundImage = `url('${slides[sAtual]}')`;
+}
+
+function expandCard(card){
+    card.style.transform = 'scale(1.05)'
+    card.style.boxShadow = '0 8px 16px rgba(0,0,0,0.3)'
+}
+
+function shinkCard(card){
+    card.style.transform = 'scale(1)'
+    card.style.boxShadow = 'none'
+}
+
